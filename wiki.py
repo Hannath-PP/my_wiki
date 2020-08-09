ae32f1bd83f59ca2149aaa51fb06ce41c19cbdaf
Hannath-PP <hannathpp998@gmail.com>
2020-08-09 18:49:00 +0200

    Create a hello world web app

--- wiki.py
+++ wiki.py
@@ -1,6 +1,7 @@
 import os
 
-pages_path='pages/'
+pages_path='pages'
+
 def save_page(title,content):
 	file_name=title+'.txt'
 	file_path=os.path.join(pages_path,file_name)
@@ -12,9 +13,12 @@ def save_page(title,content):
 def load_page(title):
 	file_name=title+'.txt'
 	file_path=os.path.join(pages_path,file_name)
-
-	f=open(file_path,'r')
-	content=f.read()
+    try:
+		f=open(file_path,'r')
+	    content=f.read()
+        1+'2'
+    except FileNotFoundError:
+    	content="No information on this topic"
 
 	return content
 
