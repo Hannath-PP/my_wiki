d10edaeb15d9f6420adbc3084eae291604c33fd0
Hannath-PP <hannathpp998@gmail.com>
2020-08-10 10:34:17 +0200

    Modifies pages_path.

--- wiki.py
+++ wiki.py
@@ -1,28 +1,30 @@
 import os
 
-pages_path='pages'
+this_folder=os.path.dirname(os.path.abspath(__file__))
+pages_path=os.path.join(this_folder,'pages')
 
 def save_page(title,content):
 	file_name=title+'.txt'
-	file_path=os.path.join(pages_path,file_name)
-
+	file_path=os.path.join(pages_path, file_name)
 	f=open(file_path,'w+')
 	f.write(content)
 	print("saved")
 
 def load_page(title):
 	file_name=title+'.txt'
-	file_path=os.path.join(pages_path,file_name)
-    try:
+	file_path=os.path.join(pages_path, file_name)
+	try:
 		f=open(file_path,'r')
-	    content=f.read()
-        1+'2'
-    except FileNotFoundError:
-    	content="No information on this topic"
+		content=f.read()
+		
+	except FileNotFoundError:
+		content="No information on this topic"
 
 	return content
 
 def list_topics():
 	pages=os.listdir('pages')
-	out=','.join(pages)
-	return out
+	topics= []
+	for page in pages:
+		topics.append(os.path.splitext(page)[0])
+	return topics
